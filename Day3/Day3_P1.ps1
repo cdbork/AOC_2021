0bd706d43c0d4babceb25d4fa5440c9c81f7096a
cdbork <cdbork@gmail.com>
2021-12-05 11:26:56 -0600

    Day3, P1

---
+++ Day3/Day3_P1.ps1
@@ -0,0 +1,28 @@
+﻿$inputs=Get-Content C:\AOC\2021\AOC_2021\Day3\Inputs.txt
+$countZero=0
+$countOne=0
+[string]$gamValue=''
+[string]$epValue=''
+
+for ($pos=0;$pos -lt $inputs[0].Length;$pos++)
+{
+    foreach ($string in $inputs) {
+        if ($string[$pos] -eq '0') {
+            $countZero++
+        } else {
+            $countOne++
+        }
+    }
+        if ($countZero -gt $countOne) {
+            $gamValue+='0'
+            $epValue+='1'            
+        } else {
+            $gamValue+='1'
+            $epValue+='0'
+        }
+        $countZero=0
+        $countOne=0
+}
+$gamint = [Convert]::ToInt32($gamValue,2)
+$epint = [Convert]::ToInt32($epValue,2)
+$gamint * $epint
