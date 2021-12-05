881fc06b6b5f3567957dd2ee1c2623f8b5255373
cdbork <cdbork@gmail.com>
2021-12-05 10:56:24 -0600

    Day2

---
+++ Day2/Day2_P1.ps1
@@ -0,0 +1,16 @@
+﻿$inputs=Get-Content C:\AOC\2021\AOC_2021\Day2\Inputs.txt
+$currHorizontal=0
+$currVert=0
+foreach ($Instruction in $inputs) {
+    $parsedComm=$Instruction.split()
+    switch ($parsedComm[0])
+    {
+        "forward" {$currHorizontal+=$parsedComm[1] }
+        "down" {$currVert+=$parsedComm[1] }
+        "up" {$currVert-=$parsedComm[1] }
+    }
+
+}
+Write-Host "Num Forward: " $currHorizontal
+Write-Host "Depth: " $currVert
+$currHorizontal * $currVert
---
+++ Day2/Day2_P2.ps1
@@ -0,0 +1,17 @@
+﻿$inputs=Get-Content C:\AOC\2021\AOC_2021\Day2\Inputs.txt
+$currHorizontal=0
+$currVert=0
+$aim=0
+foreach ($Instruction in $inputs) {
+    $parsedComm=$Instruction.split()
+    switch ($parsedComm[0])
+    {
+        "forward" {$currHorizontal+=[int]$parsedComm[1]; $currVert+=([int]$parsedComm[1]*$aim) }
+        "down" {$aim+=[int]$parsedComm[1] }
+        "up" {$aim-=[int]$parsedComm[1] }
+    }
+
+}
+Write-Host "Num Forward: " $currHorizontal
+Write-Host "Depth: " $currVert
+$currHorizontal * $currVert
