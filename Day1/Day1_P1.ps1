099a478499f51048ba1dba43913777bbc3f991b1
cdbork <cdbork@gmail.com>
2021-12-05 10:22:29 -0600

    Day1

---
+++ Day1/Day1_P1.ps1
@@ -0,0 +1,28 @@
+﻿$inputs=Get-Content C:\AOC\2021\Day1\Inputs.txt
+$currvalue=0
+$nextvalue=0
+$numIncrease=0
+$numDecrease=0
+$numFlat=0
+$initVal=$true
+
+foreach ($measure in $inputs) {
+    $currvalue=$nextvalue
+    $nextvalue=$measure
+
+    if ($initVal -eq $false)
+    {
+        $diffVal=$nextValue-$currValue
+        if ($diffVal -lt 0) {
+            $numDecrease++
+        } elseif ($diffVal -gt 0) {
+            $numIncrease++
+        } else {
+            $numFlat++
+        }
+    }
+    $initVal=$false
+}
+Write-Host "Num Increase: " $numIncrease
+Write-Host "Num Decrease: " $numDecrease
+Write-Host "Num Flat: " $numFlat
---
+++ Day1/Day1_P2.ps1
@@ -0,0 +1,30 @@
+﻿$inputs=Get-Content C:\AOC\2021\Day1\Inputs.txt
+$posA=0
+$posB=1
+$posC=2
+$sumCurr=0
+$sumNext=0
+$numIncrease=0
+$numDecrease=0
+$numFlat=0
+$sumNext=[int]$inputs[$posA]+[int]$inputs[$posB]+[int]$inputs[$posC]
+
+while ($posC -le $inputs.Length) {
+$posA++
+$posB++
+$posC++
+$sumCurr=$sumNext
+$sumNext=[int]$inputs[$posA]+[int]$inputs[$posB]+[int]$inputs[$posC]
+
+$diffVal=$sumNext-$sumCurr
+if ($diffVal -lt 0) {
+   $numDecrease++
+} elseif ($diffVal -gt 0) {
+    $numIncrease++
+} else {
+    $numFlat++
+}
+}
+Write-Host "Num Increase: " $numIncrease
+Write-Host "Num Decrease: " $numDecrease
+Write-Host "Num Flat: " $numFlat
